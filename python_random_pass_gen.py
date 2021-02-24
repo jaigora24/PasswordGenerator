93058871b562f72adf244d9bcb4afa9b4726aac2
simran-creator <79596796+simran-creator@users.noreply.github.com>
2021-02-24 22:53:10 +0530

    Create python_random_pass_gen.py

---
+++ python_random_pass_gen.py
@@ -0,0 +1,9 @@
+import random 
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+=!@#$%^&*"
+length_of_pass = int(input("num of letters:"))
+def pass_generator(num):
+    password = ''
+    for i in range(num):
+        password += random.choice(chars)
+    return password
+print(pass_generator(length_of_pass))
