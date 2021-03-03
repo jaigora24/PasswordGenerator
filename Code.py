eb678548b034635164207daa44a2837f35080908
Jai Gora <Jaigora24@gmail.com>
2021-03-04 00:36:08 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,7 +1,9 @@
-import string
-import random
-
-def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
-	return ''.join(random.choice(chars) for _ in range(size))
-
-print(pw_gen(int(input('How many characters in your password?'))))
+import random 
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+=!@#$%^&*"
+length_of_pass = int(input("num of letters:"))
+def pass_generator(num):
+    password = ''
+    for i in range(num):
+        password += random.choice(chars)
+    return password
+print(pass_generator(length_of_pass))
